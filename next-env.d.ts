5c4036ab80f64b7ea3118bd05d2b382a6ac03b0b
bhumi6211-crypto <bhumi6211@gmail.com>
2026-05-30 11:49:21 +0530

    Delete next-env.d.ts

--- next-env.d.ts
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-600 to-indigo-800 p-4">
-      {children}
-    </div>
-  );
-}
